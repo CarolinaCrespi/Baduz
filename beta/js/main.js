0b25e185e9cc0fcf66e3a0806ac948f1a7b20d88
Carolina Crespi <79015467+CarolinaCrespi@users.noreply.github.com>
2025-08-25 19:06:22 +0200

    Update main.js

--- beta/js/main.js
+++ beta/js/main.js
@@ -2,7 +2,7 @@ import MazeScene from './scene.js';
 import { saveGameSnapshot } from './storage.js';
 
 const game = new Phaser.Game({
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   width: 1000,
   height: 700,
   canvas: document.getElementById('gameCanvas'),
